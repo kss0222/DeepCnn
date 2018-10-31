2d36733cd406fa003b4e1c17a155ada705805bca
EstherKim <43005722+kss0222@users.noreply.github.com>
2018-10-31 12:33:14 +0900

    Cifar10 예측모델을 학습
    http://mazdah.tistory.com/815?category=598657  참조

--- cifar10_train.py
+++ cifar10_train.py
@@ -32,6 +32,7 @@ data set, compile the program and train the model.
 
 http://tensorflow.org/tutorials/deep_cnn/
 """
+# python 2와 3의 호환을 위한 것
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -42,13 +43,13 @@ import time
 import tensorflow as tf
 
 import cifar10
-
+# FLAGS.train_dir은 '/tmp/cifar10_train’라는 값
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 100000,
+tf.app.flags.DEFINE_integer('max_steps', 100000, // 학습 step maximum값
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
@@ -58,37 +59,38 @@ tf.app.flags.DEFINE_integer('log_frequency', 10,
 
 def train():
   """Train CIFAR-10 for a number of steps."""
-  with tf.Graph().as_default():
-    global_step = tf.train.get_or_create_global_step()
+  with tf.Graph().as_default():  # 그래프 관련 변수 전역으로 선언
+    global_step = tf.train.get_or_create_global_step() # step 카운트
 
     # Get images and labels for CIFAR-10.
     # Force input pipeline to CPU:0 to avoid operations sometimes ending up on
     # GPU and resulting in a slow down.
-    with tf.device('/cpu:0'):
-      images, labels = cifar10.distorted_inputs()
+    with tf.device('/cpu:0'): # 첫번째 cpu에서 학습 사용 GPU는 cifar10_multi_gpu_train.py 참조
+      images, labels = cifar10.distorted_inputs() # 학습에 사용할 배치 크기 (image와 label) 가져옴
 
     # Build a Graph that computes the logits predictions from the
     # inference model.
-    logits = cifar10.inference(images)
+    logits = cifar10.inference(images)  # 학습 모델 생성 cifar10.py안의 inference()함수
 
     # Calculate loss.
-    loss = cifar10.loss(logits, labels)
+    loss = cifar10.loss(logits, labels) # 손실값 계산 cifar10.py
 
     # Build a Graph that trains the model with one batch of examples and
     # updates the model parameters.
     train_op = cifar10.train(loss, global_step)
 
-    class _LoggerHook(tf.train.SessionRunHook):
+    class _LoggerHook(tf.train.SessionRunHook): #tf.train.MonitoredTrainingSession에 사용하기 위한 로그 hooker
+      # # MonitoredTrainingSession.run() 호출에 대한 로그들을 hooking
       """Logs loss and runtime."""
-
-      def begin(self):
+# session 이용 시 처음 한 번 호출되는 함수
+      def begin(self):  
         self._step = -1
         self._start_time = time.time()
-
+# run() 함수가 호출되기 전에 호출되는 함수
       def before_run(self, run_context):
         self._step += 1
         return tf.train.SessionRunArgs(loss)  # Asks for loss value.
-
+# run() 함수가 호출된 후에 호출되는 함수
       def after_run(self, run_context, run_values):
         if self._step % FLAGS.log_frequency == 0:
           current_time = time.time()
@@ -104,6 +106,7 @@ def train():
           print (format_str % (datetime.now(), self._step, loss_value,
                                examples_per_sec, sec_per_batch))
 
+  # Hook를 이용한 로그 관리, 오류 발생시 복구 처리 등
     with tf.train.MonitoredTrainingSession(
         checkpoint_dir=FLAGS.train_dir,
         hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
@@ -112,14 +115,18 @@ def train():
         config=tf.ConfigProto(
             log_device_placement=FLAGS.log_device_placement)) as mon_sess:
       while not mon_sess.should_stop():
+  
+  # 학습 수행
         mon_sess.run(train_op)
 
 
 def main(argv=None):  # pylint: disable=unused-argument
-  cifar10.maybe_download_and_extract()
+  cifar10.maybe_download_and_extract() # CIFAR-10 데이터를 다운로드 받아 저장. cifar10.py 참조
+# 학습 수행 중의 로그를 저장할 디렉토리 생성. 기존에 동일 디렉토리가 있다면 삭제 후 생성.
   if tf.gfile.Exists(FLAGS.train_dir):
     tf.gfile.DeleteRecursively(FLAGS.train_dir)
   tf.gfile.MakeDirs(FLAGS.train_dir)
+  # 학습 함수 호출
   train()
 
 
